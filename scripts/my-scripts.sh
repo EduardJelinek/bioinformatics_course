7ce19e10fdcd9bad14810d4348bb14a5a6befdcb
U-KCLAD\k21083247 <k21083247@KCL9ZPMBX3.kclad.ds.kcl.ac.uk>
2025-03-06 16:24:54 +0000

    tested: my-script.sh directories created

--- scripts/my-scripts.sh
+++ scripts/my-scripts.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env/ bash
+#!/usr/bin/bash
+mkdir -p analysis docs data
 touch ${my_directory}/README.md
-echo "# ${my_directory} >> ${my_directory}/README.md
+echo "# ${my_directory}" >> ${my_directory}/README.md
